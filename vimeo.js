0516a5b84950449b83e61bf2beda5e096b4a8e8a
thebear008 <gregoryloncle@gmail.com>
2018-05-27 23:12:39 -0400

    Update vimeo.js

--- vimeo.js
+++ vimeo.js
@@ -14,11 +14,36 @@
 
     'use strict';
 
-    function makeButton(my_string) {
+    function makeButton() {
         var my_button = document.createElement('button')
         my_button.innerHTML = 'GED info'
         my_button.onclick = function() {
-            alert(my_string)
+
+            var my_json = JSON.parse(document.querySelectorAll("script[type='application/ld+json']")[0].innerHTML)
+            var vimeo_author = my_json[0].author.name.replace( /[#!<>:"\/\\|?*]/g, '' ).replace(/ /g, '_' )
+            var vimeo_url = my_json[0].url.replace( /[#!<>:"\/\\|?*]/g, '' ).replace(/[^0-9]/g, '' )
+            var vimeo_name = my_json[0].name.replace( /[#!<>:"\/\\|?*]/g, '' ).replace(/ /g, '_' )
+            var year_upload = my_json[0].uploadDate.substring(0, 4)
+
+            var my_h1 = document.getElementsByTagName('h1')[0]
+            var my_span = document.createElement('span')
+            my_span.setAttribute('id', 'my-ged-span')
+            var my_string = document.createTextNode(vimeo_author + "|" + vimeo_url + "|" + vimeo_name + "|" + year_upload)
+
+
+
+            my_h1.appendChild(my_span);
+            my_span.appendChild(my_string)
+
+            var q_selector = document.querySelector('#my-ged-span');
+            // create a Range object
+            var range = document.createRange();
+            // set the Node to select the "range"
+            range.selectNode(q_selector);
+            // add the Range to the set of window selections
+            window.getSelection().addRange(range);
+            document.execCommand('copy');
+            alert('copy in clipboard')
         }
         return my_button
     }
@@ -28,18 +53,22 @@
     {
 
         var my_json = JSON.parse(document.querySelectorAll("script[type='application/ld+json']")[0].innerHTML)
-        var vimeo_author = my_json[0].author.name.replace( /[<>:"\/\\|?*]/g, '' ).replace(/ /g, '_' )
-        var vimeo_url = my_json[0].url.replace( /[<>:"\/\\|?*]/g, '' ).replace(/[^0-9]/g, '' )
-        var vimeo_name = my_json[0].name.replace( /[<>:"\/\\|?*]/g, '' ).replace(/ /g, '_' )
+        var vimeo_author = my_json[0].author.name.replace( /[#!<>:"\/\\|?*]/g, '' ).replace(/ /g, '_' )
+        var vimeo_url = my_json[0].url.replace( /[#!<>:"\/\\|?*]/g, '' ).replace(/[^0-9]/g, '' )
+        var vimeo_name = my_json[0].name.replace( /[#!<>:"\/\\|?*]/g, '' ).replace(/ /g, '_' )
         var year_upload = my_json[0].uploadDate.substring(0, 4)
 
-        var my_h1 = document.getElementsByTagName('h1')[0]
-        var my_string = vimeo_author + "|" + vimeo_url + "|" + vimeo_name + "|" + year_upload
+
+        //my_span.innerHTML = my_string
         //my_h1.innerHTML += "<span style='color:red'>" + my_string + "</span>"
 
         // make download button
-        var button = makeButton(my_string);
-        my_h1.appendChild(button)
+        var my_button = makeButton();
+        var my_h1 = document.getElementsByTagName('h1')[0]
+        my_h1.appendChild(my_button);
+
+
+
 
     }
 
