1f40f911c60daaa00572c09235ce60609bf55b18
Greg <gregoryloncle@gmail.com>
2018-09-03 23:11:47 -0400

    first try with instagram

---
+++ instagram.js
@@ -0,0 +1,20 @@
+// document.getElementsByTagName('time')[0].attributes.datetime : 
+
+//document.getElementsByTagName('time')[0].attributes.datetime.value
+//"2018-08-28T11:06:47.000Z"
+
+
+
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+// ... give time for script to load, then type (or see below for non wait option)
+jQuery.noConflict();
+
+jQuery('header a')[0].attributes.title
+title="motionsk8"
+
+window.location.href
+
+window.location.href.split('/')[4]
+"BnBS8aQAUnZ"
