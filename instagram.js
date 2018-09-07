386cfafa4317e97e5c6a1cdad6d746b80d81d99c
Greg <gregoryloncle@gmail.com>
2018-09-06 20:39:08 -0400

    v1 instagram

--- instagram.js
+++ instagram.js
@@ -1,20 +1,74 @@
-// document.getElementsByTagName('time')[0].attributes.datetime : 
+// ==UserScript==
+// @name         instagram_pattern_file
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @include https://www.instagram.com/*
+// @author       thebear008
+// @match        https://chrome.google.com/webstore/category/extensions?hl=fr
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
 
-//document.getElementsByTagName('time')[0].attributes.datetime.value
-//"2018-08-28T11:06:47.000Z"
+(function() {
+    'use strict';
+
+        function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function makeButton() {
+        var my_button = document.createElement('button')
+        my_button.innerHTML = 'GED info'
+        my_button.onclick = function() {
+
+        var insta_author = jQuery('header a')[0].innerHTML
+        var insta_url = window.location.href.split('/')[4]
+        var year_upload = document.getElementsByTagName('time')[0].attributes.datetime.value.split("-")[0]
+
+            var my_h1 = document.getElementsByTagName('article')[0]
+            var my_span = document.createElement('span')
+            my_span.setAttribute('id', 'my-ged-span')
+            my_span.style.color = 'white'
+            my_span.style.fontSize = '1px'
+            var my_string = document.createTextNode(insta_author + "=" + insta_url  + "=" + year_upload)
+
+            my_h1.appendChild(my_span);
+            my_span.appendChild(my_string)
+
+            var q_selector = document.querySelector('#my-ged-span');
+            // create a Range object
+            var range = document.createRange();
+            // set the Node to select the "range"
+            range.selectNode(q_selector);
+            // add the Range to the set of window selections
+            window.getSelection().addRange(range);
+            document.execCommand('copy');
+            window.getSelection().empty();
+
+        }
+        return my_button
+    }
 
 
 
-var jq = document.createElement('script');
-jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-document.getElementsByTagName('head')[0].appendChild(jq);
-// ... give time for script to load, then type (or see below for non wait option)
-jQuery.noConflict();
+    // wait for player to be ready and set up periodic video check
+    async function setup()
+    {
+        await sleep(2000);
 
-jQuery('header a')[0].attributes.title
-title="motionsk8"
+        var insta_author = jQuery('header a')[0].innerHTML
+        var insta_url = window.location.href.split('/')[4]
+        var year_upload = document.getElementsByTagName('time')[0].attributes.datetime.value.split("-")[0]
 
-window.location.href
+        // make download button
+        var my_button = makeButton();
+        var my_h1 = document.getElementsByTagName('article')[0]
+        my_h1.appendChild(my_button);
 
-window.location.href.split('/')[4]
-"BnBS8aQAUnZ"
+    }
+
+    // start looking for video player
+    setup();
+
+})();
